eac2aa3c1ad158fb215fb94295574f13fb79caa7
otk2625 <xorud8531@gmail.com>
2021-01-26 11:36:23 +0900

    바꿈

--- src/TestDB.java
+++ src/TestDB.java
@@ -7,23 +7,7 @@ public class TestDB {
 
 	public static void main(String[] args) {
 		
-		String sql = "INSERT INTO user(username, password, email, address, userRole, createDate) VALUES(?,?,?,?, 'USER', now())";
-		Connection conn = DB.getConnection();
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, dto.getUsername());
-			pstmt.setString(2, dto.getPassword());
-			pstmt.setString(3, dto.getEmail());
-			pstmt.setString(4, dto.getAddress());
-			int result = pstmt.executeUpdate();
-			return result;
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			DB.close(conn, pstmt);
-		}
-		return -1;
+		
 
 	}
 
